2e20fa45733877ae328497facead6b1381037510
moikapy <moikapy@moikaslookout.com>
2025-05-22 18:45:31 -0400

    refactor: update configuration and improve Supabase client creation
    - Changed string quotes in next.config.ts for consistency.
    - Added new environment variables for Supabase integration in next.config.ts.
    - Simplified the Supabase client creation logic in supabase_server.ts by removing unnecessary async/await and improving header management.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa';
+import withPWA from "next-pwa";
 
 const next_config = {
   reactStrictMode: true,
@@ -8,12 +8,14 @@ const next_config = {
   env: {
     STRIPE_PRICING_TABLE_ID: process.env.STRIPE_PRICING_TABLE_ID,
     STRIPE_PUB_KEY: process.env.STRIPE_PUB_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   },
 };
 
 export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 })(next_config);
--- src/lib/supabase_server.ts
+++ src/lib/supabase_server.ts
@@ -5,15 +5,13 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 export async function create_clerk_supabase_client_ssr(): Promise<SupabaseClient> {
   const auth_obj = await auth();
   const clerk_token = await auth_obj.getToken({ template: 'supabase' });
-  return await createClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       global: {
-        fetch: async (url, options = {}) => {
-          const headers = new Headers(options?.headers);
-          headers.set("Authorization", `Bearer ${clerk_token}`);
-          return fetch(url, { ...options, headers });
+        headers: {
+          Authorization: `Bearer ${clerk_token}`,
         },
       },
     }
