46aeb76d1e05be34542606201b4005ee063e6db1
moikapy <moikapy@moikaslookout.com>
2025-05-30 14:47:12 -0400

    Enhance Sidebar component with user authentication and improved layout
    - Integrated SignedIn and UserButton components from Clerk for user authentication in the sidebar.
    - Updated the layout to include a title and reorganized navigation sections for better user experience.
    - Added support links and feedback options to enhance user interaction and accessibility.

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import Image from "next/image";
 import { track } from "@vercel/analytics";
-import { useUser } from "@clerk/nextjs";
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
 
 /**
  * Sidebar component for Studio App.
@@ -29,154 +29,145 @@ export default function Sidebar({ open = false, on_close }: { open?: boolean; on
         aria-hidden={!open}
       />
       <aside
-        className={`hidden md:flex min-h-screen w-45 lg:w-64 max-w-full bg-base-200 border-r border-base-300 flex-col z-40 transition-transform duration-200  overflow-y-auto`}
+        className={`hidden md:flex flex-col justify-between min-h-screen w-45 lg:w-64 max-w-full bg-base-200 border-r border-base-300 flex-col z-40 transition-transform duration-200  overflow-y-auto`}
         aria-label="Sidebar navigation"
       >
-        {/* Close button for mobile */}
-        <div className="flex md:hidden justify-end p-2">
-          <button
-            className="btn btn-ghost btn-square"
-            onClick={on_close}
-            aria-label="Close sidebar"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+        <div className="p-3 border-b border-base-300">
+          <h2 className="text-base font-medium text-center p-4 font-mono">
+            Studio Moikas
+          </h2>
+          <nav className=" border-base-300 p-4" aria-label="Main navigation">
+            <ul className="menu menu-xs rounded-box w-full">
+              <li>
+                <Link
+                  href="/tools"
+                  className="justify-start font-bold"
+                  aria-label="Tools Home"
+                >
+                  <span className="text-base font-medium">Home</span>
+                </Link>
+              </li>
+              {/* Add more tool links here as needed */}
+            </ul>
+          </nav>
+          <div className="p-6 border-b border-base-300">
+            <p className="text-sm font-bold tracking-tight text-primary">
+              Tools
+            </p>
+          </div>
+          <nav className="flex flec-col p-4" aria-label="Main tools">
+            <ul className="menu menu-lg rounded-box w-full">
+              <li>
+                <Link
+                  href="/tools/create"
+                  className="justify-start"
+                  aria-label="Image Generator tool"
+                >
+                  <span className="text-base font-medium text-md">Create</span>
+                </Link>
+              </li>
+              {/* Add more tool links here as needed */}
+              <li>
+                <Link
+                  href="/tools/text-analyzer"
+                  className="justify-start"
+                  aria-label="Image Generator tool"
+                >
+                  <span className="text-base font-medium text-md">
+                    Text Analyzer
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/tools/video-effects"
+                  className="justify-start"
+                  aria-label="Video Generator tool"
+                >
+                  <span className="text-base font-medium text-md">
+                    Video Generator
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="p-6 border-b border-base-300">
+            <p className="text-sm font-bold tracking-tight text-primary">
+              Support
+            </p>
+          </div>
+          <nav className="flex flex-col p-4" aria-label="Main tools">
+            <ul className="menu menu-lg rounded-box w-full">
+              <li>
+                <a
+                  href="https://discord.gg/DnbkrC8"
+                  className="justify-start"
+                  aria-label="Community Discord"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    track("Sidebar Community Clicked", {
+                      timestamp: new Date().toISOString(),
+                    })
+                  }
+                >
+                  <span className="flex items-center gap-2 text-md">
+                    {/* Discord icon */}
+                    <Image
+                      src="/Discord.svg"
+                      alt="Discord"
+                      width={24}
+                      height={24}
+                    />
+                    Community
+                  </span>
+                </a>
+              </li>
+              <li>
+                <Link
+                  href="https://x.com/moikas_official"
+                  className="justify-start"
+                  aria-label="Help and News"
+                  onClick={() =>
+                    track("Sidebar Help/News Clicked", {
+                      timestamp: new Date().toISOString(),
+                    })
+                  }
+                >
+                  <span className="flex items-center gap-2 text-md">
+                    {/* Question mark icon */}
+                    <Bell />
+                    News
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="w-full text-left rounded text-md px-4 py-2 font-medium bg-base hover:bg-base-100 transition-colors duration-150"
+                  aria-label="Send Feedback"
+                  onClick={() => set_feedback_open(true)}
+                >
+                  Feedback
+                </button>
+              </li>
+              <li>
+                <button
+                  className="w-full text-left rounded text-md px-4 py-2 font-medium bg-base hover:bg-base-100 transition-colors duration-150"
+                  aria-label="Report Bug"
+                  onClick={() => set_report_bug_open(true)}
+                >
+                  Report Bug
+                </button>
+              </li>
+              {/* Add more tool links here as needed */}
+            </ul>
+          </nav>
         </div>
-        <nav className=" border-base-300 p-4" aria-label="Main navigation">
-          <ul className="menu menu-xs rounded-box w-full">
-            <li>
-              <Link
-                href="/tools"
-                className="justify-start font-bold"
-                aria-label="Tools Home"
-              >
-                <span className="text-base font-medium">Home</span>
-              </Link>
-            </li>
-            {/* Add more tool links here as needed */}
-          </ul>
-        </nav>
-        <div className="p-6 border-b border-base-300">
-          <p className="text-sm font-bold tracking-tight text-primary">Tools</p>
+        <div className="p-6 flex justify-center border-b border-base-300">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
         </div>
-        <nav className="flex flec-col p-4" aria-label="Main tools">
-          <ul className="menu menu-lg rounded-box w-full">
-            <li>
-              <Link
-                href="/tools/create"
-                className="justify-start"
-                aria-label="Image Generator tool"
-              >
-                <span className="text-base font-medium text-md">Create</span>
-              </Link>
-            </li>
-            {/* Add more tool links here as needed */}
-            <li>
-              <Link
-                href="/tools/text-analyzer"
-                className="justify-start"
-                aria-label="Image Generator tool"
-              >
-                <span className="text-base font-medium text-md">
-                  Text Analyzer
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/tools/video-effects"
-                className="justify-start"
-                aria-label="Video Generator tool"
-              >
-                <span className="text-base font-medium text-md">Video Generator</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="p-6 border-b border-base-300">
-          <p className="text-sm font-bold tracking-tight text-primary">
-            Support
-          </p>
-        </div>
-        <nav className="flex flex-col p-4" aria-label="Main tools">
-          <ul className="menu menu-lg rounded-box w-full">
-            <li>
-              <a
-                href="https://discord.gg/DnbkrC8"
-                className="justify-start"
-                aria-label="Community Discord"
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={() =>
-                  track("Sidebar Community Clicked", {
-                    timestamp: new Date().toISOString(),
-                  })
-                }
-              >
-                <span className="flex items-center gap-2 text-md">
-                  {/* Discord icon */}
-                  <Image
-                    src="/Discord.svg"
-                    alt="Discord"
-                    width={24}
-                    height={24}
-                  />
-                  Community
-                </span>
-              </a>
-            </li>
-            <li>
-              <Link
-                href="https://x.com/moikas_official"
-                className="justify-start"
-                aria-label="Help and News"
-                onClick={() =>
-                  track("Sidebar Help/News Clicked", {
-                    timestamp: new Date().toISOString(),
-                  })
-                }
-              >
-                <span className="flex items-center gap-2 text-md">
-                  {/* Question mark icon */}
-                  <Bell />
-                  News
-                </span>
-              </Link>
-            </li>
-            <li>
-              <button
-                className="w-full text-left rounded text-md px-4 py-2 font-medium bg-base hover:bg-base-100 transition-colors duration-150"
-                aria-label="Send Feedback"
-                onClick={() => set_feedback_open(true)}
-              >
-                Feedback
-              </button>
-            </li>
-            <li>
-              <button
-                className="w-full text-left rounded text-md px-4 py-2 font-medium bg-base hover:bg-base-100 transition-colors duration-150"
-                aria-label="Report Bug"
-                onClick={() => set_report_bug_open(true)}
-              >
-                Report Bug
-              </button>
-            </li>
-            {/* Add more tool links here as needed */}
-          </ul>
-        </nav>
       </aside>
       <FeedbackMenu
         open={feedback_open}
